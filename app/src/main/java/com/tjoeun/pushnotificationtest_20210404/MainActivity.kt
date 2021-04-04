830e7ba33a3956baf60b292fec6e27ee6fefc2ea
조경진 <cho881020@gmail.com>
2021-04-04 16:44:14 +0900

    메인화면 토큰값 (임시방편) 추출 + 알림 오면 핸들러 이용해서 토스트 띄우기

--- app/src/main/java/com/tjoeun/pushnotificationtest_20210404/MainActivity.kt
+++ app/src/main/java/com/tjoeun/pushnotificationtest_20210404/MainActivity.kt
@@ -2,10 +2,22 @@ package com.tjoeun.pushnotificationtest_20210404
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.google.firebase.iid.FirebaseInstanceId
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+            Log.d("디바이스토큰값", FirebaseInstanceId.getInstance().token!!)
+        }, 5000)
+
+
     }
 }
--- app/src/main/java/com/tjoeun/pushnotificationtest_20210404/fcm/MyFCMService.kt
+++ app/src/main/java/com/tjoeun/pushnotificationtest_20210404/fcm/MyFCMService.kt
@@ -1,6 +1,9 @@
 package com.tjoeun.pushnotificationtest_20210404.fcm
 
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -9,8 +12,15 @@ class MyFCMService : FirebaseMessagingService() {
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
 
-        Log.d("푸시알림-제목", p0.notification!!.title!!)
-        Log.d("푸시알림-내용", p0.notification!!.body!!)
+//        Log.d("푸시알림-제목", p0.notification!!.title!!)
+//        Log.d("푸시알림-내용", p0.notification!!.body!!)
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.post {
+            Toast.makeText(applicationContext, p0.notification!!.title!!, Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 }
